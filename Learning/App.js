a357c83e1fa6682dbff9b220b2595ce66325e7f6
Brightlaz <lazbright1@gmail.com>
2022-06-15 12:48:47 -0700

    Update learning
    Done learning props and maps, It time to work on a project

--- Learning/App.js
+++ Learning/App.js
@@ -1,33 +1,55 @@
-// import Card from "./components/Card"
+// // import Card from "./components/Card"
+
+// // export default function App() {
+// //             // <Hero />
+// //     return (
+// //         <div>
+// //             <Navbar />
+// //             <Card 
+// //                 img="katie-zaferes.png"
+// //                 rating="5.0"
+// //                 reviewCount={6}
+// //                 country="USA"
+// //                 title="Life Lessons with Katie Zaferes"
+// //                 price={136}
+// //             />
+// //         </div>
+// //     )
+
+
+// // export default function App() {
+// //     const cards = data.map(item => {
+// //         return (
+// //             <Card
+// //                 key={item.id}
+// //                 item={item} OR 
+// //                 {...item}
+// //             />
+// //         )
+// //     })        
+
+// //             // <Hero />
+// //     return (
+// //         <div>
+// //             <Navbar />
+// //             <section className="cards-list">
+// //                 {cards}
+// //             </section>
+// //         </div>
+// //     )
+// // }
+
+// import React from "react"
+// import Joke from "./Joke"
+// import jokesData from "./jokesData"
 
 // export default function App() {
-//             // <Hero />
+//     const jokeElements = jokesData.map(joke => {
+//         return <Joke setup={joke.setup} punchline={joke.punchline} />
+//     })
 //     return (
 //         <div>
-//             <Navbar />
-//             <Card 
-//                 img="katie-zaferes.png"
-//                 rating="5.0"
-//                 reviewCount={6}
-//                 country="USA"
-//                 title="Life Lessons with Katie Zaferes"
-//                 price={136}
-//             />
+//             {jokeElements}
 //         </div>
 //     )
-
-import React from "react"
-import Joke from "./Joke"
-import jokesData from "./jokesData"
-
-export default function App() {
-    const jokeElements = jokesData.map(joke => {
-        return <Joke setup = { joke.setup }
-        punchline = { joke.punchline }
-        />
-    })
-    return ( <
-        div > { jokeElements } <
-        /div>
-    )
-}
+// }
