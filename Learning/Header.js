ebf1e190842477907eb1ee214d88dd85d296b517
Brightlaz <lazbright1@gmail.com>
2022-06-28 17:43:19 -0700

    Learned about form and state

--- Learning/Header.js
+++ Learning/Header.js
@@ -40,3 +40,55 @@ import { process_params } from "express/lib/router"
 //         </div>
 //     )
 // }
+
+// const isGoingOut = true;
+// let answer = isGoingOut ? 'Yes' : 'No';
+
+// import React from "react"
+
+// export default function App() {
+//     const [starWarsData, setStarWarsData] = React.useState({})
+//     const [count, setCount] = React.useState(1)
+
+//     React.useEffect(function() {
+//          async function getMemes() {
+//     const res = await fetch(`https://swapi.dev/api/people/${count}`)
+//     const data = await res.json()
+//     setStarWarsData(data)
+// }
+// getMemes()
+// }, [])
+//   
+
+//     return (
+//         <div>
+//             <h2>The count is {count}</h2>
+//             <button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
+//             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+//         </div>
+//     )
+// }
+// import React from "react"
+
+// export default function WindowTracker() {
+
+//     const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
+
+//     React.useEffect(() => {
+//         function watchWidth() {
+//             console.log("Setting up...")
+//             setWindowWidth(window.innerWidth)
+//         }
+
+//         window.addEventListener("resize", watchWidth)
+
+//         return function() {
+//             console.log("Cleaning up...")
+//             window.removeEventListener("resize", watchWidth)
+//         }
+//     }, [])
+
+//     return (
+//         <h1>Window width: {windowWidth}</h1>
+//     )
+// }
